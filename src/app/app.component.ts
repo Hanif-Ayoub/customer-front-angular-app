a4bab1e655de2a1668e1280f2c0421c1075c7e65
Hanif-Ayoub <haniayur@gmail.com>
2023-12-25 13:58:40 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {KeycloakProfile} from "keycloak-js";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'angular-front-app';
